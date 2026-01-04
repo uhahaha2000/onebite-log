58f5a4f93e287e6ef8d9f0acbfe2fa5ee1f77a2f
uhahaha2000 <uhahaha2000@naver.com>
2026-01-05 00:21:40 +0900

    7-2

--- src/types.ts
+++ src/types.ts
@@ -3,8 +3,6 @@ import { type Database } from "@/database.types";
 export type PostEntity = Database["public"]["Tables"]["post"]["Row"];
 export type ProfileEntity = Database["public"]["Tables"]["profile"]["Row"];
 
-export type Post = PostEntity & { author: ProfileEntity };
-
 export type UseMutationCallback = {
   onSuccess?: () => void;
   onError?: (error: Error) => void;
