74288d6526a0a435e3e404dd5a601f30ff04f666
joen <joen@dunamu.com>
2026-01-06 13:18:13 +0900

    7-3 이미지 업로드 캐로셀

--- src/components/modal/post-editor-modal.tsx
+++ src/components/modal/post-editor-modal.tsx
@@ -1,10 +1,20 @@
 import { Button } from "@/components/ui/button";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
 import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
 import { useCreatePost } from "@/hooks/mutations/post/use-create-post";
 import { usePostEditorModal } from "@/store/post-editor-modal";
 import { ImageIcon } from "lucide-react";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, type ChangeEvent } from "react";
 import { toast } from "sonner";
+
+type Image = {
+  file: File;
+  previewUrl: string;
+};
 
 export default function PostEditorModal() {
   const { isOpen, close } = usePostEditorModal();
@@ -20,7 +30,9 @@ export default function PostEditorModal() {
   });
 
   const [content, setContent] = useState("");
+  const [images, setImages] = useState<Image[]>([]);
   const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const fileInputRef = useRef<HTMLInputElement>(null);
 
   const handleCloseModal = () => {
     close();
@@ -29,6 +41,22 @@ export default function PostEditorModal() {
   const handleCreatePostClick = () => {
     if (content.trim() === "") return;
     createPost(content);
+  };
+
+  const handleSelectImages = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const files = Array.from(e.target.files);
+      files.forEach((file) => {
+        setImages((prev) => [
+          ...prev,
+          {
+            file,
+            previewUrl: URL.createObjectURL(file),
+          },
+        ]);
+      });
+    }
+    e.target.value = ""; // 입력값 초기화
   };
 
   useEffect(() => {
@@ -57,7 +85,19 @@ export default function PostEditorModal() {
           className="max-h-125 min-h-25 focus:outline-none"
           placeholder="무슨 일이 있었나요?"
         />
+        <input
+          ref={fileInputRef}
+          type="file"
+          accept="image/*"
+          multiple
+          className="hidden"
+          onChange={handleSelectImages}
+        />
+
         <Button
+          onClick={() => {
+            fileInputRef.current?.click();
+          }}
           disabled={isCreatePostPending}
           variant={"outline"}
           className="cursor-pointer"
@@ -65,6 +105,24 @@ export default function PostEditorModal() {
           <ImageIcon />
           이미지 추가
         </Button>
+
+        {images.length > 0 && (
+          <Carousel>
+            <CarouselContent>
+              {images.map((image, index) => (
+                <CarouselItem key={image.previewUrl} className="basis-2/5">
+                  <div>
+                    <img
+                      src={image.previewUrl}
+                      alt={`Selected ${index + 1}`}
+                      className="h-full w-full rounded-sm object-cover "
+                    />
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+          </Carousel>
+        )}
         <Button
           disabled={isCreatePostPending}
           onClick={handleCreatePostClick}
