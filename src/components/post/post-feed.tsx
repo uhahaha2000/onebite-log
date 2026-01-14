69e0a176e2544aefe4e8e953f83e12b03e608b52
uhahaha2000 <uhahaha2000@naver.com>
2026-01-15 00:33:54 +0900

    스크롤 하단 감지 추가

--- src/components/post/post-feed.tsx
+++ src/components/post/post-feed.tsx
@@ -2,9 +2,18 @@ import Fallback from "@/components/fallback";
 import Loader from "@/components/loader";
 import PostItem from "@/components/post/post-item";
 import { usePostsData } from "@/hooks/mutations/post/use-posts-data";
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
 
 export default function PostFeed() {
   const { data, error, isPending } = usePostsData();
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      // 데이터 추가
+    }
+  }, [inView]);
 
   if (error) return <Fallback />;
 
@@ -15,6 +24,7 @@ export default function PostFeed() {
       {data.map((post) => (
         <PostItem key={post.id} {...post} />
       ))}
+      <div ref={ref} className="h-4 bg-amber-800"></div>
     </div>
   );
 }
