8fe14537cd92bf7974121606f2762ed4b8bb65b2
uhahaha2000 <uhahaha2000@naver.com>
2026-02-01 23:05:34 +0900

    7-14 캐싱 설정하기 3. 캐시 조작

--- src/hooks/mutations/post/use-delete-post.ts
+++ src/hooks/mutations/post/use-delete-post.ts
@@ -1,9 +1,12 @@
 import { deleteImageInPath } from "@/api/image";
 import { deletePost } from "@/api/post";
+import { QUERY_KEYS } from "@/lib/constants";
 import type { UseMutationCallback } from "@/types";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useDeletePost(callbacks?: UseMutationCallback) {
+  
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: deletePost,
     onSuccess: async(deletedPost) => {
@@ -11,7 +14,12 @@ export function useDeletePost(callbacks?: UseMutationCallback) {
       
       if (deletedPost.image_urls && deletedPost.image_urls.length > 0) {
         await deleteImageInPath(`${deletedPost.author_id}/${deletedPost.id}`);
-        }
+      }
+      
+      queryClient.resetQueries({
+        queryKey: QUERY_KEYS.post.list,
+      });
+      
 
     },
     onError: (error) => {
