57334cce0dcaa06fcfe3a8118975dee81f22ae68
uhahaha2000 <uhahaha2000@naver.com>
2026-02-08 22:48:42 +0900

    8-2 좋아요 기능

--- src/api/post.ts
+++ src/api/post.ts
@@ -94,3 +94,16 @@ export async function deletePost(id: number) {
   
   return data;
 }
+
+export async function togglePostLike({postId, userId}: {postId: number, userId: string}) { 
+  const { data, error } = await supabase.rpc(
+    "toggle_post_like",
+    {
+      p_post_id: postId,
+      p_user_id: userId,
+    }
+  );
+
+  if (error) throw error;
+  return data;
+}
--- src/components/post/like-post-button.tsx
+++ src/components/post/like-post-button.tsx
@@ -1,9 +1,30 @@
+import useTogglePostLike from "@/hooks/mutations/post/use-toggle-post-like";
+import { useSession } from "@/store/session";
 import { HeartIcon } from "lucide-react";
+import { use } from "react";
+import { toast } from "sonner";
 
 export default function LikePostButton({ id, likeCount }: { id: number; likeCount: number }) {
   
+  const { mutate: togglePostLike } = useTogglePostLike({
+    onError: (error) => {
+      toast.error(`좋아요 요청에 실패했습니다`, {
+        position: "top-center",
+      });
+    }
+  });
+
+  const session = useSession();
+
+  const handleLikeClick = () => {
+    togglePostLike({
+      postId: id,
+      userId: session!.user.id
+    }
+    );
+  }
   return (
-    <div className="hover:bg-muted flex cursor-pointer items-center gap-2 rounded-xl border-1 p-2 px-4 text-sm">
+    <div className="hover:bg-muted flex cursor-pointer items-center gap-2 rounded-xl border-1 p-2 px-4 text-sm" onClick={handleLikeClick}>
       <HeartIcon className="h-4 w-4" />
       <span>{likeCount}</span>
     </div>
