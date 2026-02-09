3c0e2ed0103df87a33940512f06655a382e1c6ed
uhahaha2000 <uhahaha2000@naver.com>
2026-02-09 23:15:13 +0900

    프로필 버튼 구현하기

--- src/api/post.ts
+++ src/api/post.ts
@@ -2,13 +2,19 @@ import { uploadImage } from "@/api/image";
 import supabase from "@/lib/supabase";
 import type { PostEntity } from "@/types";
 
-export async function fetchPosts({ from, to , userId}: {from:number, to: number, userId: string}) {
-  const { data, error } = await supabase
+export async function fetchPosts({ from, to , userId, authorId}: {from:number, to: number, userId: string, authorId?: string}) {
+  const request = supabase
     .from("post")
     .select("*, author: profile!author_id (*), myLiked: like!post_id (*)")
     .eq("like.user_id", userId)
     .order('created_at', { ascending: false })
-    .range(from,to);
+    .range(from, to);
+  
+  if (authorId) { 
+    request.eq("author_id", authorId);
+  }
+
+  const { data, error } = await request;
 
   if (error) throw error;
   return data.map((post) => ({
