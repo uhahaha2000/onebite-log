e1856daa30d43dea57054dc12c7e5137e1244ed4
uhahaha2000 <uhahaha2000@naver.com>
2026-02-18 23:04:36 +0900

    프로필 수정 팝업 ui

--- src/components/profile/edit-profile-button.tsx
+++ src/components/profile/edit-profile-button.tsx
@@ -1,8 +1,16 @@
 import { Button } from "@/components/ui/button";
+import { useOpenProfileEditorModal } from "@/store/profile-editor-modal";
 
 export default function EditProfileButton() {
+
+  const openProfileEditorModal = useOpenProfileEditorModal();
+
   return (
-    <Button variant="secondary" className="cursor-pointer">
+    <Button
+      variant="secondary"
+      className="cursor-pointer"
+      onClick={openProfileEditorModal}
+    >
       프로필 수정
     </Button>
   );
