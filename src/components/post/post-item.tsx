5ea164e08baf5d8a01b9825af322f71802a35463
uhahaha2000 <uhahaha2000@naver.com>
2026-02-21 22:46:55 +0900

    포스트 상세

--- src/components/post/post-item.tsx
+++ src/components/post/post-item.tsx
@@ -17,18 +17,26 @@ import Fallback from "@/components/fallback";
 import LikePostButton from "@/components/post/like-post-button";
 import { Link } from "react-router";
 
-export default function PostItem({ postId }: { postId: number }) {
+export default function PostItem({
+  postId,
+  type,
+}: {
+  postId: number;
+  type: "FEED" | "DETAIL";
+}) {
   const session = useSession();
   const useId = session?.user.id;
 
-  const { data: post, isPending, error } = usePostByIdData({ postId, type: "FEED" })
-  if (isPending) return <Loader />
-  if (error) return <Fallback />
+  const { data: post, isPending, error } = usePostByIdData({ postId, type });
+  if (isPending) return <Loader />;
+  if (error) return <Fallback />;
 
   const isMine = useId === post.author.id;
 
   return (
-    <div className="flex flex-col gap-4 border-b pb-8">
+    <div
+      className={`flex flex-col gap-4 pb-8 ${type === "FEED" && "border-b"}`}
+    >
       {/* 1. 유저 정보, 수정/삭제 버튼 */}
       <div className="flex justify-between">
         {/* 1-1. 유저 정보 */}
@@ -62,11 +70,17 @@ export default function PostItem({ postId }: { postId: number }) {
       </div>
 
       {/* 2. 컨텐츠, 이미지 캐러셀 */}
-      <div className="flex cursor-pointer flex-col gap-5">
+      <div className="flex flex-col gap-5">
         {/* 2-1. 컨텐츠 */}
-        <div className="line-clamp-2 break-words whitespace-pre-wrap">
-          {post.content}
-        </div>
+        {type === "FEED" ? (
+          <Link to={`/post/${post.id}`}>
+            <div className="line-clamp-2 break-words whitespace-pre-wrap">
+              {post.content}
+            </div>
+          </Link>
+        ) : (
+          <div className="break-words whitespace-pre-wrap">{post.content}</div>
+        )}
 
         {/* 2-2. 이미지 캐러셀 */}
         <Carousel>
@@ -88,13 +102,21 @@ export default function PostItem({ postId }: { postId: number }) {
       {/* 3. 좋아요, 댓글 버튼 */}
       <div className="flex gap-2">
         {/* 3-1. 좋아요 버튼 */}
-        <LikePostButton id={post.id} likeCount={post.like_count} isLiked={post.isLiked} />
+        <LikePostButton
+          id={post.id}
+          likeCount={post.like_count}
+          isLiked={post.isLiked}
+        />
 
         {/* 3-2. 댓글 버튼 */}
-        <div className="hover:bg-muted flex cursor-pointer items-center gap-2 rounded-xl border-1 p-2 px-4 text-sm">
-          <MessageCircle className="h-4 w-4" />
-          <span>댓글 달기</span>
-        </div>
+        {type === "FEED" && (
+          <Link to={`/post/${post.id}`}>
+            <div className="hover:bg-muted flex cursor-pointer items-center gap-2 rounded-xl border-1 p-2 px-4 text-sm">
+              <MessageCircle className="h-4 w-4" />
+              <span>댓글 달기</span>
+            </div>
+          </Link>
+        )}
       </div>
     </div>
   );
--- src/pages/post-detail-page.tsx
+++ src/pages/post-detail-page.tsx
@@ -1,3 +1,17 @@
+import PostItem from "@/components/post/post-item";
+import { Navigate, useParams } from "react-router";
+
 export default function PostDetailPage() {
-  return <div>PostDetailPage</div>;
+  const params = useParams();
+  const postId = params.postId;
+
+  if (!postId) {
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <div>
+      <PostItem postId={Number(postId)} type="DETAIL" />
+    </div>
+  );
 }
