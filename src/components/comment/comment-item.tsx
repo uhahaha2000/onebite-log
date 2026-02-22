f65316a997f453c7b5313423e925900ed12068bd
uhahaha2000 <uhahaha2000@naver.com>
2026-02-22 23:05:33 +0900

    댓글 수정 기능 추가 및 관련 컴포넌트 수정

--- src/components/comment/comment-item.tsx
+++ src/components/comment/comment-item.tsx
@@ -2,8 +2,19 @@ import { Link } from "react-router";
 import defaultAvatar from "@/assets/default-avatar.png";
 import type { Comment } from "@/types";
 import { formatTimeAgo } from "@/lib/time";
+import { useSession } from "@/store/session";
+import { useState } from "react";
+import CommentEditor from "@/components/comment/comment-editor";
 
 export default function CommentItem(props: Comment) {
+  const session = useSession();
+  const [isEditing, setIsEditing] = useState(false);
+  const toggleIsEditing = () => {
+    setIsEditing(!isEditing);
+  };
+
+  const isMine = session?.user.id === props.author_id;
+
   return (
     <div className={"flex flex-col gap-8 border-b pb-5"}>
       <div className="flex items-start gap-4">
@@ -17,7 +28,18 @@ export default function CommentItem(props: Comment) {
         </Link>
         <div className="flex w-full flex-col gap-2">
           <div className="font-bold">{props.author.nickname}</div>
-          <div>{props.content}</div>
+
+          {isEditing ? (
+            <CommentEditor
+              type="EDIT"
+              commentId={props.id}
+              initialContent={props.content}
+              onClose={toggleIsEditing}
+            />
+          ) : (
+            <div>{props.content}</div>
+          )}
+
           <div className="text-muted-foreground flex justify-between text-sm">
             <div className="flex items-center gap-2">
               <div className="cursor-pointer hover:underline">댓글</div>
@@ -25,9 +47,18 @@ export default function CommentItem(props: Comment) {
               <div>{formatTimeAgo(props.created_at)}</div>
             </div>
             <div className="flex items-center gap-2">
-              <div className="cursor-pointer hover:underline">수정</div>
-              <div className="bg-border h-[13px] w-[2px]"></div>
-              <div className="cursor-pointer hover:underline">삭제</div>
+              {isMine && (
+                <>
+                  <div
+                    className="cursor-pointer hover:underline"
+                    onClick={toggleIsEditing}
+                  >
+                    수정
+                  </div>
+                  <div className="bg-border h-[13px] w-[2px]"></div>
+                  <div className="cursor-pointer hover:underline">삭제</div>
+                </>
+              )}
             </div>
           </div>
         </div>
